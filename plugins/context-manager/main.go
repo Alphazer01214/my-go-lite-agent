a4526a328dab179835107897f493c17b5caacd76
Alphazer01214 <1374734991@qq.com>
2026-09-25 20:00:46 +0800

    tmp

--- plugins/context-manager/main.go
+++ plugins/context-manager/main.go
@@ -1,1 +1,38 @@
 package contextmanager
+
+import (
+	"json"
+)
+
+type chatMessage struct {
+	Role       string     `json:"role"`
+	Content    string     `json:"content"`
+	ToolCalls  []toolCall `json:"tool_calls,omitempty"`
+	ToolCallID string     `json:"tool_call_id,omitempty"`
+}
+
+type toolCall struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Function struct {
+		Name      string `json:"name"`
+		Arguments string `json:"arguments"`
+	} `json:"function"`
+}
+
+type tool struct {
+	Type     string `json:"type"`
+	Function struct {
+		Name        string          `json:"name"`
+		Description string          `json:"description"`
+		Parameters  json.RawMessage `json:"parameters"`
+	} `json:"function"`
+}
+
+type History struct {
+	messages []chatMessage `json:"messages"`
+}
+
+func compact(messages []chatMessage) []chatMessage {
+
+}
---
+++ pluginsdk/webui.go
@@ -0,0 +1,4 @@
+package pluginsdk
+
+type Layout struct {
+}
